247400a2359867c781207ab4446bfa8864a76e5a
Bommer5 <nathan23.brico@gmail.com>
2025-11-29 15:40:55 +0100

    perfectionnement de l'interface

--- code/main.py
+++ code/main.py
@@ -1,25 +1,19 @@
-#main
-import skimage
-from matplotlib import pyplot as plt
-from extraction import selectionner_images
-from trie import detect_layout_from_image
+# main.py
 
+import skimage
+# from matplotlib import pyplot as plt # Plus nécessaire
+from extraction import selectionner_images
+# from trie import detect_layout_from_image # Plus nécessaire ici
 
 def main():
     print("--- Programme Principal ---")
     
+    # La fonction sélectionne et lance l'interface
+    # L'analyse se fait dans l'interface lorsque l'utilisateur clique sur "VALIDER"
+    selectionner_images() 
+    
+    # Le programme se termine lorsque l'utilisateur ferme la fenêtre.
+    print("Fermeture du programme.")
 
-    images = selectionner_images()
-
-    if not images:
-        print("Aucune image sélectionnée.")
-    else:
-        print(f"\n{len(images)} images prêtes pour l'analyse :")
-        for img_path in images:
-            print(f" -> {img_path}")
-            
-        # ICI : Tu appelles ta future fonction de détection
-        # detecter_azerty_qwerty(images)
-    print(detect_layout_from_image(images))
 if __name__ == "__main__":
     main()
