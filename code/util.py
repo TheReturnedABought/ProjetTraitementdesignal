158f34472226beec69c17264951c6c38a97e6441
Girboux <thomasgirboux@gmail.com>
2025-11-29 16:02:26 +0100

    Added sobel++ from theo's code

--- code/util.py
+++ code/util.py
@@ -15,13 +15,19 @@ def sobel(gray):
     H, W = gray.shape
     G = np.zeros((H, W))
 
-    for y in range(1, H - 1):
-        for x in range(1, W - 1):
-            region = gray[y - 1:y + 2, x - 1:x + 2]
-            gx = np.sum(region * Sx)
-            gy = np.sum(region * Sy)
-            G[y, x] = np.sqrt(gx * gx + gy * gy)
+    gx = (
+            gray[:-2, :-2] * Sx[0, 0] + gray[:-2, 1:-1] * Sx[0, 1] + gray[:-2, 2:] * Sx[0, 2] +
+            gray[1:-1, :-2] * Sx[1, 0] + gray[1:-1, 1:-1] * Sx[1, 1] + gray[1:-1, 2:] * Sx[1, 2] +
+            gray[2:, :-2] * Sx[2, 0] + gray[2:, 1:-1] * Sx[2, 1] + gray[2:, 2:] * Sx[2, 2]
+    )
+
+    gy = (
+            gray[:-2, :-2] * Sy[0, 0] + gray[:-2, 1:-1] * Sy[0, 1] + gray[:-2, 2:] * Sy[0, 2] +
+            gray[1:-1, :-2] * Sy[1, 0] + gray[1:-1, 1:-1] * Sy[1, 1] + gray[1:-1, 2:] * Sy[1, 2] +
+            gray[2:, :-2] * Sy[2, 0] + gray[2:, 1:-1] * Sy[2, 1] + gray[2:, 2:] * Sy[2, 2]
+    )
 
     # Normalize to 0-255
-    G = (G / G.max()) * 255
+    G = np.sqrt(gx * gx + gy * gy)
+    G = (G - G.min()) / (G.max() - G.min()) * 255
     return G.astype(np.uint8)
